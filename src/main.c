520193157a9a8b4003647ec1f62b59337c262ae1
Mauricio Bernardini <mausissio@gmail.com>
2017-11-06 15:54:58 -0200

    Scheduler Finalizado,eu acho

--- src/main.c
+++ src/main.c
@@ -3,11 +3,24 @@
 int main() {
   int a[5];
   int n;
+  int tarefas;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (int i=1; i<=n; i++) 
+  {
+	tarefas = 0;
+	for(int j=0;j<5;j++)
+		{
+			if(i % a[j]==0)
+				{
+					printf("%d", j);
+					tarefas++;
+				}
+
+		}
+	if(tarefas == 0) printf("-");
+	printf("\n");
   }
 
   return 0;
